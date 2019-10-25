c09a0d4a503d62093a1de864edc71b4c205b52be
dinadhandayani <dinadwihandayani1@gmail.com>
2019-10-25 17:20:04 +0700

    Update project : Pilih daerah v4

---
+++ app/src/main/java/com/didapteam/project/education/UserData.java
@@ -0,0 +1,11 @@
+package com.didapteam.project.education;
+
+public class UserData {
+    static String nama= "";
+    static String email ="";
+    static String bidang="";
+    static String daerah="";
+    static String pilihan="";
+    static Boolean status=false;
+    static String imgUrl="";
+}
